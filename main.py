a07fed8c8b90557d6735df5540b9d2fc7e9452be
Nickson Magambo <126444800+NickMurimi@users.noreply.github.com>
2023-06-08 16:34:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+NameFood = input("what's your food? ")
+NamePlant = input("what's your type of plant? ")
+NameCooking = input("what's the method of cooking? ")
+BurnedFood = input("what word describes burned food? ")
+DiyItem = input("what's a DIY item? ")
+print(NameCooking, NameFood, "with", BurnedFood, NamePlant, "on a bed of", DiyItem)
